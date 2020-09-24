289a729f4d523271bacc73d7ad8ec9ba061191fb
peterb <peter.boreland@matillion.com>
2020-09-24 21:10:14 +0100

    Create flashcard store

---
+++ app/src/main/java/com/example/myapplication/model/FlashCard.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.model;
+
+import java.util.UUID;
+
+public class FlashCard {
+    private final String topic;
+    private final String question;
+    private final String answer;
+    private final String uuid;
+
+    public FlashCard(String question, String answer, String topic) {
+        this.question = question;
+        this.answer = answer;
+        this.topic = topic;
+        this.uuid = UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/tools/CardResourceStore.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.tools;
+
+import android.content.SharedPreferences;
+
+import com.example.myapplication.model.FlashCard;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardResourceStore {
+    public static final String CARD_FILE_NAME = "flash_card_resource";
+
+    public static void saveFlashCard(FlashCard flashCard, SharedPreferences sharedPreferences) {
+        Gson gson = new Gson();
+        String serializedCard = gson.toJson(flashCard);
+        sharedPreferences.edit().putString(flashCard.getUuid(), serializedCard).apply();
+    }
+
+    public static List<FlashCard> getFlashCards(SharedPreferences sharedPreferences) {
+        Gson gson = new Gson();
+        List<String> allRawCards = (List<String>) sharedPreferences.getAll().values();
+        List<FlashCard> listOfCards = new ArrayList<>();
+        for (int i = 0; i < allRawCards.size(); i++) {
+            FlashCard card = gson.fromJson(allRawCards.get(i), FlashCard.class);
+            listOfCards.add(card);
+        }
+        return listOfCards;
+    }
+
+    public static String getCardFileName() {
+        return CARD_FILE_NAME;
+    }
+}
