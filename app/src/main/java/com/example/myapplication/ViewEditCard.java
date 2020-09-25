6e1f05937869274abec1fa86b49c529fe6501810
peterb <peter.boreland@matillion.com>
2020-09-25 20:44:47 +0100

    Update MainActivity to launch ViewEditCard on click to add new card, and update ViewEditCard to save card on save.

--- app/src/main/java/com/example/myapplication/ViewEditCard.java
+++ app/src/main/java/com/example/myapplication/ViewEditCard.java
@@ -2,7 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication.model.FlashCard;
+import com.example.myapplication.tools.CardResourceStore;
 
 public class ViewEditCard extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class ViewEditCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_edit_card);
+    }
+
+    public void onClickSaveCard(View view) {
+        String topic = ((TextView) findViewById(R.id.card_topic_text)).getText().toString();
+        String quesiton = ((TextView) findViewById(R.id.card_question_text)).getText().toString();
+        String answer = ((TextView) findViewById(R.id.card_answer_text)).getText().toString();
+        String resourceFilePath = CardResourceStore.getCardFileName();
+        SharedPreferences sharedPreferences = getSharedPreferences(resourceFilePath, Context.MODE_PRIVATE);
+        FlashCard flashCard = new FlashCard(quesiton, answer, topic);
+        CardResourceStore.saveFlashCard(flashCard, sharedPreferences);
+        launchMain();
+    }
+
+    public void launchMain()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
