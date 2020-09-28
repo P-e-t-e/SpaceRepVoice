411d39a7c6303afb2acff0152fe7161808a1a452
peterb <peter.boreland@matillion.com>
2020-09-28 20:21:50 +0100

    Add TTS working demo

--- app/src/main/java/com/example/myapplication/PlayQuestionsActivity.java
+++ app/src/main/java/com/example/myapplication/PlayQuestionsActivity.java
@@ -3,12 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class PlayQuestionsActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_questions);
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i != TextToSpeech.ERROR)
+                {
+                    tts.setLanguage(Locale.UK);
+                }
+            }
+        });
+    }
+
+    public void onClickStartButton(View view)
+    {
+        Toast.makeText(this, "Speaking!", Toast.LENGTH_SHORT).show();
+        tts.speak("testing tts", TextToSpeech.QUEUE_FLUSH, null);
     }
 }
