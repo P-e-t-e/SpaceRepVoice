e29244688843896808018c438d70d8efc9ac6a95
peterb <peter.boreland@matillion.com>
2020-09-28 21:38:00 +0100

    Add user speech to text (app control) working demo

--- app/src/main/java/com/example/myapplication/PlayQuestionsActivity.java
+++ app/src/main/java/com/example/myapplication/PlayQuestionsActivity.java
@@ -2,16 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.List;
 import java.util.Locale;
 
 public class PlayQuestionsActivity extends AppCompatActivity {
 
     TextToSpeech tts;
+    private static final int SPEECH_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +36,31 @@ public class PlayQuestionsActivity extends AppCompatActivity {
     {
         Toast.makeText(this, "Speaking!", Toast.LENGTH_SHORT).show();
         tts.speak("testing tts", TextToSpeech.QUEUE_FLUSH, null);
+        displaySpeechRecognizer();
     }
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            tts.speak("Did you say " + spokenText, TextToSpeech.QUEUE_FLUSH, null);
+            // Do something with spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
